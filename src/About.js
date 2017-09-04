230d96ac63861176f6486fa3a20edaa2eeec4297
Ramin Barati <rekino@gmail.com>
2017-09-04 14:09:53 +0430

    - about  page nav first draft - started animations - navigator hover animation is done

--- src/About.js
+++ src/About.js
@@ -1,14 +1,26 @@
 import React, { Component } from 'react';
 import './About.css';
+import logo from './logo.svg';
 
 class About extends Component {
-  render() {
-    return (
-      <div>
-        Aboutpage
-      </div>
-    )
-  }
+    toggleMenu() {
+
+    }
+    render() {
+        return (
+            <div className="app">
+                <nav className="nav">
+                    <div className="menu">
+                        <div className="logo">
+                            <img src={logo} alt="Logo" />
+                        </div>
+                        <button className="button back"><i className="fa fa-arrow-left"></i></button>
+                        <button className="button upper pathway" onClick={this.toggleMenu.bind(this)}>Menu <i className="fa fa-bars"></i></button>
+                    </div>
+                </nav>
+            </div>
+        )
+    }
 }
 
 export default About;
