051f4f1be1445c36b99c3a51f6851e303cac0196
Ramin Barati <rekino@gmail.com>
2017-09-13 16:58:23 +0430

    - added new fonts - moved shared css classes from About.css to index.css - merged two paragraphs in About.js - changed fonts - changed nav background - completed H2 component

--- src/About.js
+++ src/About.js
@@ -3,30 +3,32 @@ import './About.css';
 import Frame from './Components/Frame';
 import image from './temp/image-04.jpg';
 import H2 from './Components/H2';
-
+//f8f8f8
 class About extends Component {
     render() {
         return (
             <Frame>
-                <div className="aboutus arvo">
-                    <h1 className="title">
-                        We Build<br />
-                        Mechanized Parking Lots
+                <div className="aboutus page-inner arvo">
+                    <h1 className="title color-primary">
+                        We build<br />
+                        mechanized parking lots
                     </h1>
                     <img className="cat-image" src={image} alt="optimpark" />
-                    <H2>New Generation of parking</H2>
-                    <p>Optimpark manufactures a new generation of mechanized parking. A solution to
+                    <div className="sege">
+                        <H2>New generation of parking</H2>
+                        <p className="text">Optimpark manufactures a new generation of mechanized parking. A solution to
                     consume minimum of resources to aquire optimum outcomes. We are experts in saving
                     maximum of space for car parking by reducing production costs and optimizing space
-                    consumption.</p>
-                    <p>Optimpark company produces smart mechanized parking lots for public and private
+                    consumption. Optimpark company produces smart mechanized parking lots for public and private
                     implementation. We create efficient solutions to customize, economize and optimize
                     your parking space.</p>
-                    <div className="iso">
-                        <span>ISO 9001-2015</span>
-                        <span>ISO 14001-2015</span>
-                        <span>CE</span>
+                        <div className="iso">
+                            <span>ISO 9001-2015</span>
+                            <span>ISO 14001-2015</span>
+                            <span>CE</span>
+                        </div>
                     </div>
+
                 </div>
             </Frame>
         )
--- src/Components/H2/H2.js
+++ src/Components/H2/H2.js
@@ -1,5 +1,6 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import './H2.css';
 
 export default class H2 extends Component {
   static propTypes = {
@@ -8,7 +9,7 @@ export default class H2 extends Component {
 
   render() {
     return (
-      <h2 className="sub"><span>{this.props.children}</span></h2>
+      <h2 className="sub color-alt"><span>{this.props.children}</span></h2>
     )
   }
 }
