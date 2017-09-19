441137e0462ba60a40beecd3929358c265d125af
Ramin Barati <rekino@gmail.com>
2017-09-19 12:16:59 +0430

    - Customization page added

---
+++ src/Customization.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './Customization.css';
+import Frame from './Components/Frame';
+import image from './assets/new_image.jpg';
+import H2 from './Components/H2';
+
+class About extends Component {
+    render() {
+        return (
+            <Frame>
+                <div className="customization page-inner arvo">
+                    <h1 className="title color-primary full-width">
+                        Custom-Built Parking Lot
+                    </h1>
+                </div>
+            </Frame>
+        )
+    }
+}
+
+export default About;
