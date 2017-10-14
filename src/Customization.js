e524c040fe8b1d27238638de6e5c9a4b4c035c5d
Ramin Barati <rekino@gmail.com>
2017-10-14 14:36:19 +0330

    - points in Customization first draft

--- src/Customization.js
+++ src/Customization.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './Customization.css';
 import { Frame, H2, Comein } from './Components';
+import cust1 from './temp/stroke-grey.png';
+import cust2 from './temp/stroke-blue.png';
 import cust5 from './assets/poster.jpg';
 
 class Customization extends Component {
@@ -31,28 +33,53 @@ class Customization extends Component {
                             </p></Comein>
                         </div>
                     </div>
+                    <div className="third-width col2 container-points">
+                        <div className="col image-points">
+                            <img src={cust1} alt=""/>
+                        </div>
+                        <div className="col text-points">
+                            <div className="point connected">
+                                <em><span></span>Body dimensions</em>
+                                <p className="text"><strong>W:</strong> 6m <strong>H:</strong> 12m</p>
+                            </div>
+                            <div className="point connected">
+                                <em><span></span>Palette dimenssions</em>
+                                <p className="text"><strong>W:</strong> 2.2m <strong>H:</strong> 4m <strong>D:</strong> 2m</p>
+                            </div>
+                            <div className="point">
+                                <em>Weight capacity</em>
+                                <p className="text">Capable of holding up to <strong>2.5 tons</strong></p>
+                            </div>
+                            <div className="point connected">
+                                <em><span></span>Car entrance</em>
+                                <p className="text"><strong>W:</strong> 3m <strong>H:</strong> 2m</p>
+                            </div>
+                        </div>
+                    </div>
                     <div className="full-width bg-primary">
                         <div className="third-width just-sub">
                             <Comein><H2>Flexibility in customization</H2></Comein>
                         </div>
                         <div className="third-width col3">
                             <div className="col">
-                                <img src="" alt="wireframe" aria-hidden="true" />
+                                <img src={cust2} alt="wireframe" aria-hidden="true" />
                             </div>
                             <div className="col">
-                                <img src="" alt="wireframe" aria-hidden="true" />
+                                <img src={cust2} alt="wireframe" aria-hidden="true" />
                             </div>
                             <div className="col">
-                                <img src="" alt="wireframe" aria-hidden="true" />
+                                <img src={cust2} alt="wireframe" aria-hidden="true" />
                             </div>
                         </div>
                     </div>
-                    <div className="just-sub third-width">
-                        <Comein><H2>Complete your parking lot <br className="desktop"/>
-                        with a modern looking facade</H2></Comein>
+                    <div className="full-width bg-white">
+                        <div className="just-sub third-width">
+                            <Comein><H2>Complete your parking lot <br className="desktop" />
+                                with a modern looking facade</H2></Comein>
+                        </div>
                     </div>
                     <div className="full-width cat-image">
-                        <img src={cust5} alt="customization" aria-hidden="true"/>
+                        <img src={cust5} alt="customization" aria-hidden="true" />
                     </div>
                 </div>
             </Frame>
