1434d0883420bcd37ba77705b648e4c8790e021b
Ramin Barati <rekino@gmail.com>
2017-10-21 11:41:52 +0330

    - swapped phone img with video in Contact

--- src/Contact.js
+++ src/Contact.js
@@ -1,24 +1,18 @@
 import React, { Component } from 'react';
 import Frame from './Components/Frame';
 import './Contact.css';
-import vidMobile from './assets/mobile.mp4';
 import imgMobile from './assets/mobile.svg';
 
 export default class Contact extends Component {
     componentDidMount() {
         window.scrollTo(0, 0);
-        this.refs.video.play();
     }
 
     render() {
         return (
             <Frame>
                 <div className="contacts ubuntu">
-                    <video className="desktop" ref="video" width="100%">
-                        <source src={vidMobile} type="video/mp4" />
-                        Your browser does not support the video tag.
-                    </video>
-                    <img className="mobile" width="80%" src={imgMobile} alt="mobile" />
+                    <img className="phone" width="80%" src={imgMobile} alt="mobile" />
                     <div className="desktop vsep"></div>
                     <div className="contact">
                         <span>Contact us:</span>
